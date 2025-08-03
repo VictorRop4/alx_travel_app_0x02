64217ef3a1e37e2c880b49d544fa7ca229f0e208
VictorRop4 <victorrop99@gmail.com>
2025-08-03 06:15:11 -0700

    API development for Listings and Bookings

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -2,6 +2,26 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+# listings/views.py
+from rest_framework import viewsets
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
+
+class ListingViewSet(viewsets.ModelViewSet):
+    """
+    A viewset that provides the standard CRUD actions for the Listing model.
+    """
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    """
+    A viewset that provides the standard CRUD actions for the Booking model.
+    """
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
 
 class ListingView(APIView):
     def get(self, request):
